e2e4507694d938a26e99535f277deb1c65c74ac4
hamadasayedmo <hh068506hamada@gmail.com>
2024-11-14 02:12:05 +0200

    useNavigate Hook

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -5,11 +5,18 @@ import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 import { useContext } from "react";
+import { useState } from "react";
 import ThemeProvider from "../context/ContextData";
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firbasee/config";
 
 const SignIn = () => {
   // use Context
   const { theme } = useContext(ThemeProvider);
+
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
 
   return (
     <div>
@@ -28,14 +35,38 @@ const SignIn = () => {
               name="email"
               placeholder="Enter Email"
               required
+              onChange={(e)=> {
+                setemail(e.target.value);
+              }}
             />
             <input
               type="password"
               name="password"
               placeholder="Pass Word Pleas"
               required
+              onChange={(e)=> {
+                setpassword(e.target.value);
+              }}
             />
-            <input type="submit" value="Sign in" className="btn" />
+            <input
+              type="submit"
+              value="Sign in"
+              className="btn"
+              onClick={(e) => {
+                e.preventDefault();
+                signInWithEmailAndPassword(auth, email, password)
+                  .then((userCredential) => {
+                    // Signed in
+                    console.log('good');
+                    const user = userCredential.user;
+                    // ...
+                  })
+                  .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                  });
+              }}
+            />
           </form>
           <div className="signin-to-signup">
             <p>
